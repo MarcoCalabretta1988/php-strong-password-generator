dbd2ce905ca03919e3aa29c1d8476adf16405b8c
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-02-08 16:32:18 +0100

    Add head includes

--- index.php
+++ index.php
@@ -23,19 +23,8 @@ if (!$number_of_char) {
 
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Strong Password Generator</title>
-
-    <!-- IMPORT BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-    <!-- IMPORT STYLE -->
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<!-- HEAD -->
+<?php include 'includes/head.php' ?>
 
 <body>
     <div class="container h-100 d-flex align-items-center justify-content-center p-5">
